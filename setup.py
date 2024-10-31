67fa863c0f5522c80bc59cca4ce6a5b25e320da9
xilax79 <xilax79@gmail.com>
2024-10-30 23:24:29 -0300

    v0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='guard_decorator',
-    version='0.1.0',
+    version='0.1.1',
     author='xilax79',
     author_email='xilax79@gmail.com',
     description='A decorator to simplify error handling in Python.',
